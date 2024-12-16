89b0ca10bef1dfe2e8fbdcf4445ba1f0cde9a2f0
Ayesh Navindu <navindu35@gmail.com>
2024-12-16 15:24:37 +0530

    Initial commit for common project structure

---
+++ src/main/java/com/springframework/boot/demo_project/service/impl/DemoServiceImpl.java
@@ -0,0 +1,4 @@
+package com.springframework.boot.demo_project.service.impl;
+
+public class DemoServiceImpl {
+}
---
+++ src/main/java/com/springframework/boot/demo_project/utils/Constants.java
@@ -0,0 +1,4 @@
+package com.springframework.boot.demo_project.utils;
+
+public class Constants {
+}
