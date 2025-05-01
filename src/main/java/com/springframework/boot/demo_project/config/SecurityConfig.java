503a3f1d47964c11a62530a06dda1ead7bf797c2
Ayesh Navindu <navindu35@gmail.com>
2025-05-01 15:34:59 +0530

    Adding Spring Boot Actuator

--- src/main/java/com/springframework/boot/demo_project/config/SecurityConfig.java
+++ src/main/java/com/springframework/boot/demo_project/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("api/**").permitAll()
                         .anyRequest().authenticated()
                 );
